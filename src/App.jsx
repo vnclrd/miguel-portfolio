968f3657bc9aa65491e9232636b78a907dc9578c
ivancalarde1301 <ivan.calarde1301@gmail.com>
2025-07-23 19:20:34 +0800

    Separate css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 import { useState } from 'react'
-import './App.css'
+import './css/App.css'
+import './css/Header.css'
+import './css/Body1.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <div class="header">
       <ul class="links">
-        <li><a href="home">HOME</a></li>
-        <li><a href="home">EXPERIENCE</a></li>
-        <li><a href="home">SKILLS</a></li>
-        <li><a href="home">EDUCATION</a></li>
-        <li><a href="home">CERTIFICATIONS</a></li>
-        <li><a href="home">ABOUT</a></li>
+        <li><a href="home">Home</a></li>
+        <li><a href="home">Experience</a></li>
+        <li><a href="home">Skills</a></li>
+        <li><a href="home">Education</a></li>
+        <li><a href="home">Certification</a></li>
+        <li><a href="home">About</a></li>
       </ul>
     </div>
+
+    <div class="body1">
+      <div class="intro">
+        <h1 class="intro-name"><b class="intro-name">Hi, I'm Miguel</b></h1>
+        <p class="intro-description">
+          Welcome to my portfolio website.
+        </p>
+      </div>
+    </div>
+
+    
+
+
+
+
+    </>
+    
   )
 }
 
