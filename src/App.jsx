06f1d3834bcc076651b5bddd9a92ff2afa1f55f8
ivancalarde1301 <ivan.calarde1301@gmail.com>
2025-07-23 19:34:48 +0800

    Setup css for other sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 import './css/App.css'
 import './css/Header.css'
-import './css/Body1.css'
+import './css/1-Home.css'
+import './css/2-Experience.css'
+import './css/3-Skills.css'
+import './css/4-Education.css'
+import './css/5-Certifications.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,15 +15,15 @@ function App() {
     <div class="header">
       <ul class="links">
         <li><a href="home">Home</a></li>
-        <li><a href="home">Experience</a></li>
-        <li><a href="home">Skills</a></li>
-        <li><a href="home">Education</a></li>
-        <li><a href="home">Certification</a></li>
-        <li><a href="home">About</a></li>
+        <li><a href="experience">Experience</a></li>
+        <li><a href="skills">Skills</a></li>
+        <li><a href="education">Education</a></li>
+        <li><a href="certifications">Certifications</a></li>
+        <li><a href="about">About</a></li>
       </ul>
     </div>
 
-    <div class="body1">
+    <div section="home" class="home">
       <div class="intro">
         <h1 class="intro-name"><b class="intro-name">Hi, I'm Miguel</b></h1>
         <p class="intro-description">
@@ -28,11 +32,15 @@ function App() {
       </div>
     </div>
 
-    
+    <div section="experience" class="experience">Experience</div>
 
+    <div section="skills" class="skills">Skills</div>
 
+    <div section="education" class="education">Education</div>
 
+    <div section="certifications" class="certifications">Certifications</div>
 
+    <div section="about" class="about"></div>
     </>
     
   )
