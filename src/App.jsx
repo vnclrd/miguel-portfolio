f86d86a22397d85c42d95f93e44ae200a37de05a
ivancalarde1301 <ivan.calarde1301@gmail.com>
2025-07-23 21:33:47 +0800

    Enhance header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import Header from './jsx/Header.jsx'
 import './css/App.css'
 import './css/Header.css'
 import './css/1-Home.css'
@@ -9,48 +9,32 @@ import './css/5-Education.css'
 import './css/6-Certifications.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <div class="header">
-      <ul class="links">
-        <li><a href="home">Home</a></li>
-        <li><a href="about">About</a></li>
-        <li><a href="experience">Experience</a></li>
-        <li><a href="skills">Skills</a></li>
-        <li><a href="education">Education</a></li>
-        <li><a href="certifications">Certifications</a></li>
-      </ul>
-    </div>
+      <Header />
 
-    <div section="home" class="home">
-      <div class="intro">
+      <div section="home" class="home">
+        <div class="intro">
 
-        <h1 class="intro-name"><b>Hi, I'm</b><b class="intro-name-color"> Miguel</b></h1>
-        <p class="intro-description">
-          Welcome to my portfolio website.
-        </p>
-        <button class="get-started">Get Started</button>
+          <h1 class="intro-name"><b>Hi, I'm</b><b class="intro-name-color"> Miguel</b></h1>
+          <p class="intro-description">
+            Welcome to my portfolio website.
+          </p>
+          <button class="get-started">Get Started</button>
+        </div>
       </div>
-    </div>
 
-    <div section="about" class="about">About</div>
+      <div section="about" class="about">About</div>
 
-    <div section="experience" class="experience">Experience</div>
+      <div section="experience" class="experience">Experience</div>
 
-    <div section="skills" class="skills">Skills</div>
+      <div section="skills" class="skills">Skills</div>
 
-    <div section="education" class="education">Education</div>
+      <div section="education" class="education">Education</div>
 
-    <div section="certifications" class="certifications">Certifications</div>
-
-    
-    </>
-    
+      <div section="certifications" class="certifications">Certifications</div>
+      </>
   )
 }
 
-export default App
-
-
+export default App;
---
+++ src/jsx/Header.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react'
+
+// define the function
+function Header() {   
+
+  // scrolled = where page is scrolled (true/false)
+  // setScrolled = function to update state
+  const [scrolled, setScrolled] = useState(false);
+
+  // useEffect() runs code when browser is loaded
+  // empty [] means it will only run once
+  useEffect(() => {
+    
+    // function that checks how far the user has scrolled
+    // setScrolled(window.scrollY > 10) checks if scroll is more than 10px
+    // if yes, setScrolled(true), otherwise setScrolled(false)
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10);
+    };
+
+    // tells browser to run handleScroll function when user scrolls
+    window.addEventListener("scroll", handleScroll)
+    
+    // cleanup function
+    // removes scroll listener when components is removed.
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  // elements of the header
+  return (
+    // style={} applies inline styles directly to the element
+    // 0.7 (true), 1 (false)
+    <div className="header"
+        style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+        minHeight: '7vh',
+        borderRadius: scrolled ? 25 : 0,
+        marginTop: scrolled ? '25px' : '0px',
+        width: scrolled ? '45%' : '100%',
+        filter: scrolled
+            ? 'drop-shadow(0 0 10px rgba(0, 0, 0, 0.15))'
+            : 'drop-shadow(0 0 25px rgba(0, 0, 0, 0.10))',
+        position: 'fixed',
+        top: 0,
+        left: '50%',
+        transform: 'translateX(-50%)',
+        transition: 'all 0.3s ease'
+    }}>
+      <ul className="links">
+        <li><a href="home">Home</a></li>
+        <li><a href="about">About</a></li>
+        <li><a href="experience">Experience</a></li>
+        <li><a href="skills">Skills</a></li>
+        <li><a href="education">Education</a></li>
+        <li><a href="certifications">Certifications</a></li>
+      </ul>
+
+    </div>
+  );
+}
+
+export default Header
