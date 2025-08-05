fe183a4c6c04b253c5573cfb414bbe8cbb499d97
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-05 13:37:33 +0800

    Add technologies

--- src/jsx/Projects.jsx
+++ src/jsx/Projects.jsx
@@ -6,42 +6,66 @@ function Projects() {
         <div className='image-container-p1'></div>
         <h1>TrainIn</h1>
         <p>Automated Attendance System</p>
-        <p class='date'>July 2025 - Present</p>
-        <p class='description'>
+        <p className='date'>July 2025 - Present</p>
+        <p className='description'>
           TrainIn is an automated attendance system designed to record and track
           the attendance of individuals at organized events. It uses NFC cards
           along with an external reader device to provide a seamless and
           efficient check-in and check-out process. The collected data can then
           be compiled into summaries and reports for easy analysis and management.
         </p>
+        <div className='technologies'>
+          <div className="tech-1">React Native</div>
+          <div className="tech-1">JavaScript</div>
+          <div className="tech-1">TypeScript</div>
+          <div className="tech-1">React Native</div>
+          <div className="tech-1">React Native</div>
+          <div className="tech-1">React Native</div>
+        </div>
       </div>
       
       <div className='project-2'>
         <div className='image-container-p2'></div>
         <h1>InstaPrint</h1>
         <p>Printing Kiosk</p>
-        <p class='date'>July 2025 - Present</p>
-        <p class='description'>
-          TrainIn is an automated attendance system designed to record and track
-          the attendance of individuals at organized events. It uses NFC cards
-          along with an external reader device to provide a seamless and
-          efficient check-in and check-out process. The collected data can then
-          be compiled into summaries and reports for easy analysis and management.
+        <p className='date'>July 2025 - Present</p>
+        <p className='description'>
+          Developed a full-stack printing kiosk that calculates document pricing
+          based on page count, color, and paper size, enabling automated,
+          affordable printing for students and faculty. Implemented hardware interaction with
+          printers, coin acceptors and change system with cashless payment with
+          GCash with integration with PayMongo.
         </p>
+        <div className='technologies'>
+          <div className="tech-1">React Native</div>
+          <div className="tech-1">JavaScript</div>
+          <div className="tech-1">TypeScript</div>
+          <div className="tech-1">React Native</div>
+          <div className="tech-1">React Native</div>
+          <div className="tech-1">React Native</div>
+        </div>
       </div>
 
       <div className='project-3'>
         <div className='image-container-p3'></div>
         <h1>Project 3</h1>
         <p>Automated Attendance System</p>
-        <p class='date'>July 2025 - Present</p>
-        <p class='description'>
+        <p className='date'>July 2025 - Present</p>
+        <p className='description'>
           TrainIn is an automated attendance system designed to record and track
           the attendance of individuals at organized events. It uses NFC cards
           along with an external reader device to provide a seamless and
           efficient check-in and check-out process. The collected data can then
           be compiled into summaries and reports for easy analysis and management.
         </p>
+        <div className='technologies'>
+          <div className="tech-1">React Native</div>
+          <div className="tech-1">JavaScript</div>
+          <div className="tech-1">TypeScript</div>
+          <div className="tech-1">React Native</div>
+          <div className="tech-1">React Native</div>
+          <div className="tech-1">React Native</div>
+        </div>
       </div>
 
     </div>
