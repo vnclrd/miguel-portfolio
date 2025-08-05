b91519a0e3868f6cfdd3dc68608b78111ea58744
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-05 14:28:15 +0800

    Update css

--- src/jsx/Projects.jsx
+++ src/jsx/Projects.jsx
@@ -14,23 +14,23 @@ function Projects() {
           efficient check-in and check-out process. The collected data can then
           be compiled into summaries and reports for easy analysis and management.
         </p>
-        <div className='technologies'>
-          <div className="tech-1">
+        <div className='tools'>
+          <div className="tool-1">
             <image className="react-js" />React Native
           </div>
-          <div className="tech-1">
+          <div className="tool-1">
             <image className="js" />JavaScript
           </div>
-          <div className="tech-1">
+          <div className="tool-1">
             <image className="ts" />TypeScript
           </div>
-          <div className="tech-1">
+          <div className="tool-1">
             <image className="html" />HTML
           </div>
-          <div className="tech-1">
+          <div className="tool-1">
             <image className="css" />CSS
           </div>
-          <div className="tech-1">
+          <div className="tool-1">
             <image className="sqlite" />SQLite
           </div>
         </div>
@@ -40,7 +40,7 @@ function Projects() {
         <div className='image-container-p2'></div>
         <h1>InstaPrint</h1>
         <p>Printing Kiosk</p>
-        <p className='date'>July 2025 - Present</p>
+        <p className='date'>September 2023 - June 2025</p>
         <p className='description'>
           Developed a full-stack printing kiosk that calculates document pricing
           based on page count, color, and paper size, enabling automated,
@@ -48,21 +48,21 @@ function Projects() {
           printers, coin acceptors and change system with cashless payment with
           GCash with integration with PayMongo.
         </p>
-        <div className='technologies'>
-          <div className="tech-1">React Native</div>
-          <div className="tech-1">JavaScript</div>
-          <div className="tech-1">TypeScript</div>
-          <div className="tech-1">React Native</div>
-          <div className="tech-1">React Native</div>
-          <div className="tech-1">React Native</div>
+        <div className='tools'>
+          <div className="tool-1">React Native</div>
+          <div className="tool-1">JavaScript</div>
+          <div className="tool-1">TypeScript</div>
+          <div className="tool-1">React Native</div>
+          <div className="tool-1">React Native</div>
+          <div className="tool-1">React Native</div>
         </div>
       </div>
 
       <div className='project-3'>
         <div className='image-container-p3'></div>
         <h1>Project 3</h1>
-        <p>Automated Attendance System</p>
-        <p className='date'>July 2025 - Present</p>
+        <p>Project 3 Subtitle</p>
+        <p className='date'>Start Date - End Date</p>
         <p className='description'>
           TrainIn is an automated attendance system designed to record and track
           the attendance of individuals at organized events. It uses NFC cards
@@ -70,13 +70,13 @@ function Projects() {
           efficient check-in and check-out process. The collected data can then
           be compiled into summaries and reports for easy analysis and management.
         </p>
-        <div className='technologies'>
-          <div className="tech-1">React Native</div>
-          <div className="tech-1">JavaScript</div>
-          <div className="tech-1">TypeScript</div>
-          <div className="tech-1">React Native</div>
-          <div className="tech-1">React Native</div>
-          <div className="tech-1">React Native</div>
+        <div className='tools'>
+          <div className="tool-1">React Native</div>
+          <div className="tool-1">JavaScript</div>
+          <div className="tool-1">TypeScript</div>
+          <div className="tool-1">React Native</div>
+          <div className="tool-1">React Native</div>
+          <div className="tool-1">React Native</div>
         </div>
       </div>
 
