bf13ed6c6a57a642b762bb9ba8a36f9985752386
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-08 17:51:17 +0800

    Update about section

--- src/jsx/About.jsx
+++ src/jsx/About.jsx
@@ -3,7 +3,7 @@ import styles from '../css/About.module.css'
 function About() {
   return (
     <div id='about' className={styles.about}>
-      <h1 className={styles.title}>About Me</h1>
+      <h1 className={styles.title}>Something About Me</h1>
 
       <div className={styles.photoContainer}>
         <div className={styles.circle} />
@@ -11,12 +11,23 @@ function About() {
       </div>
 
       <div className={styles.descriptionContainer}>
-          <p className={styles.description}>
+          <p className={styles.description1}>
             "I'm excited to learn and grow as I explore opportunities
             to apply and expand my skills in software development.
             Currently, I’m sharpening my abilities through hands-on
             projects and coding practice, while staying open to
             collaborating with like-minded professionals."
+          </p>
+          <p className={styles.description2}>
+            Hi, I’m Miguel — a passionate developer who creates clean,
+            functional, and engaging digital experiences. I build
+            responsive websites and impactful mobile apps, focusing
+            on intuitive design and seamless performance. With practical
+            experiences in JavaScript, React, and modern tools, I
+            continually sharpen my skills through personal projects
+            and coding challenges. Beyond my profession, I love running,
+            music, sneakers, keyboards, and more. Feel free to check out
+            my projects below or get in touch if you'd like to collaborate!
           </p>
       </div>
     </div>
