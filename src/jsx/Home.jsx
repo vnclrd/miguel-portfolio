c0a1416c09dd513d1fc8ca44ad4780e3662d4dd6
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-09 20:05:14 +0800

    Update home css

--- src/jsx/Home.jsx
+++ src/jsx/Home.jsx
@@ -1,18 +1,18 @@
-import styles from '../css/Home.module.css'
+import '../css/Home.css'
 import linkedinLogo from '/images/logos/linkedin.png'
 import githubLogo from '/images/logos/github.png'
 
 function Home() {
   return (
-    <div id="home" className={styles.home}>    
+    <div id="home" className='home-container'>    
       <div>
-        <h1 className={styles.introName}><b>Hi, I'm Miguel.</b></h1>
-        <p className={styles.profession}>I'm a <b>Computer Engineer</b>, a <b>Developer</b>, a <b>Graphic Designer</b>, <br></br>and an <b>Aspiring Software Engineer</b></p>
+        <h1 className='home-intro-name'><b>Hi, I'm Miguel.</b></h1>
+        <p className='home-profession'>I'm a <b>Computer Engineer</b>, a <b>Developer</b>, a <b>Graphic Designer</b>, <br></br>and an <b>Aspiring Software Engineer</b></p>
         <a href='https://www.linkedin.com/in/vnclrd/'>
-          <img src={linkedinLogo} className={styles.logoContainer} />
+          <img src={linkedinLogo} className='home-logo-container' />
         </a>
         <a href='https://github.com/vnclrd'>
-          <img src={githubLogo} className={styles.logoContainer} />
+          <img src={githubLogo} className='home-logo-container' />
         </a>
       </div>
     </div>
