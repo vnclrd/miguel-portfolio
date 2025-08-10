b3137af08c355f5d4ec470458cd5e28bf6d1ea70
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-10 21:21:42 +0800

    Update home section

--- src/jsx/About.jsx
+++ src/jsx/About.jsx
@@ -39,28 +39,6 @@ function About() {
           className='w-full h-full object-cover hover:grayscale-0 transition-all duration-300'
         />
       </div>
-
-      {/* <div className='about-description-container'>
-          <p className='about-description-1'>
-            "I'm excited to learn and grow as I explore opportunities
-            to apply and expand my skills in software development.
-            Currently, I’m sharpening my abilities through hands-on
-            projects and coding practice, while staying open to
-            collaborating with like-minded professionals."
-          </p>
-          <p className='about-description-2'>
-            Hi, I’m Miguel — a passionate developer who creates clean,
-            functional, and engaging digital experiences. I build
-            responsive websites and impactful mobile apps, focusing
-            on intuitive design and seamless performance. With practical
-            experiences in JavaScript, React, and modern tools, I
-            continually sharpen my skills through personal projects
-            and coding challenges. Beyond my profession, I love running,
-            music, sneakers, keyboards, and more. Feel free to check out
-            my projects below or get in touch if you'd like to collaborate!
-          </p>
-      </div> */}
-
     </div>
   );
 }
