8510f6218f167134e245c6ffec625a1091034bd7
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-12 00:21:59 +0800

    Update photo holder spacing

--- src/jsx/Work.jsx
+++ src/jsx/Work.jsx
@@ -4,9 +4,13 @@ function Work() {
 
         <div className='flex items-center gap-10'>
           
-          <div className='w-[400px] h-[250px] mb-50 ml-50 bg-[#b0bec5] z-1000'></div>
-          <div className='absolute w-[375px] h-[250px] mb-40 ml-45 bg-none border-[2px] border-[#00b4d8]'></div>
-          <div className='absolute w-[400px] h-[100px] bg-[#121212] ml-140 mb-50 z-1000'></div>
+          <div className='w-[400px] h-[250px] mb-60 ml-50 bg-[#b0bec5] z-1000'></div>
+          <div className='absolute w-[375px] h-[250px] mb-50 ml-45 bg-none border-[2px] border-[#00b4d8]'></div>
+          <div className='absolute w-[400px] h-[150px] bg-[#121212] text-justify ml-140 mb-60 p-3.5 z-1000'>
+            <p className='flex text-[#e0e0e0] text-[0.75rem]'>
+              TrainIn is an automated attendance system designed to record and track the attendance of individuals at organized events. It uses NFC cards along with an external reader device to provide a seamless and efficient check-in and check-out process. The collected data can then be compiled into summaries and detailed reports for easy analysis and management.
+            </p>
+          </div>
 
           <div className='w-[400px] h-[250px] mt-50 bg-[#b0bec5] z-1000'></div>
           <div className='absolute w-[375px] h-[250px] mt-60 ml-171 bg-none border-[2px] border-[#00b4d8]'></div>
