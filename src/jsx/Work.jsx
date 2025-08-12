8470eedc35d36f9945d1a7280ccf921b20c66f38
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-12 16:22:23 +0800

    Update work descriptions

--- src/jsx/Work.jsx
+++ src/jsx/Work.jsx
@@ -5,36 +5,45 @@ function Work() {
         <div className='flex items-center gap-10'>
 
           <div className='absolute w-[400px] h-[400px] mb-40 ml-50'>
-            <h1 className='text-[#e0e0e0] text-[1.5rem] font-bold'>What I've been up to...</h1>
+            <h1 className='text-[#e0e0e0] text-[1.5rem] font-bold'>What I've worked on</h1>
           </div>
           
-          <div className='w-[400px] h-[250px] mb-50 ml-50 bg-[#b0bec5] z-1000'></div>
+          <div className='w-[400px] h-[250px] mb-50 ml-50 bg-[#b0bec5] z-1000'>
+            <img 
+              src="/miguel-portfolio/images/trainin-aas.jpg"
+              alt="TrainIn"
+              className='w-full h-full object-cover'
+            />
+          </div>
           <div className='absolute w-[375px] h-[250px] mb-40 ml-45 bg-none border-[2px] border-[#00b4d8]'></div>
 
           <h1 className='absolute mb-107 ml-161 text-[#e0e0e0] text-[1.25rem] font-semibold'>TrainIn</h1>
           <h1 className='absolute mb-96 ml-161 text-[#e0e0e0] text-[0.75rem]'>Automated Attendance System</h1>
 
-          <div className='absolute w-[400px] h-[150px] bg-[#121212] text-justify ml-140 mb-50 p-3.5 z-1000'>
+          <div className='absolute w-[400px] h-[120px] bg-[#000e1e] text-justify ml-145 mb-60 p-3.5 z-1000'>
             <p className='flex text-[#e0e0e0] text-[0.75rem]'>
               TrainIn is an automated attendance system designed to record and track the attendance
               of individuals at organized events. It uses NFC cards along with an external reader
-              device to provide a seamless and efficient check-in and check-out process. The collected
-              data can then be compiled into summaries and detailed reports for easy analysis and management.
+              device to provide a seamless and efficient check-in and check-out process.
             </p>
           </div>
 
           <h1 className='absolute mt-37 ml-125 text-[#e0e0e0] text-[1.25rem] font-semibold'>InstaPrint</h1>
           <h1 className='absolute mt-48 ml-111 text-[#e0e0e0] text-[0.75rem]'>Self-Service Printing Kiosk</h1>
 
-          <div className='w-[400px] h-[250px] mt-60 bg-[#b0bec5] z-1000'></div>
+          <div className='w-[400px] h-[250px] mt-60 bg-[#b0bec5] z-1000'>
+            <img 
+              src="/miguel-portfolio/images/instaprint.jpg"
+              alt="TrainIn"
+              className='w-full h-full object-cover'
+            />
+          </div>
           <div className='absolute w-[375px] h-[250px] mt-70 ml-171 bg-none border-[2px] border-[#00b4d8]'></div>
-          <div className='absolute w-[400px] h-[175px] bg-[#121212] text-justify ml-65 mt-100 p-3.5 z-1000'>
+          <div className='absolute w-[400px] h-[120px] bg-[#000e1e] text-justify ml-65 mt-85 p-3.5 z-1000'>
             <p className='flex text-[#e0e0e0] text-[0.75rem]'>
               Developed a full-stack printing kiosk that calculates document pricing
-              based on page count, color, and paper size, enabling automated, affordable
-              printing for students and faculty. Integrated Python with Flask for back-end
-              development and utilized WebSocket-based payment tracking. Implemented hardware
-              interaction with printers, coin acceptors and change system. As well as cashless
+              based on page count, color, and paper size. Utilized Python + Flask for
+              back-end and utilized WebSocket-based payment tracking as well as cashless
               payment with GCash with integration with PayMongo.
             </p>
           </div>
