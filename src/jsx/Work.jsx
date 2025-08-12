742987aac3ee401602e1bde584fefd05fa7806a1
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-12 17:14:17 +0800

    Add comments to work section

--- src/jsx/Work.jsx
+++ src/jsx/Work.jsx
@@ -1,13 +1,14 @@
 function Work() {
   return(
     <div id="work" className='flex h-[100vh] bg-[#0d1b2a]'>
-
         <div className='flex items-center gap-10'>
+          {/* Section Title */}
+          <h1 className='absolute w-[400px] h-[400px] mb-40 ml-50 text-[#e0e0e0] text-[1.5rem] font-bold'>
+            What I've worked on
+          </h1>
 
-          <div className='absolute w-[400px] h-[400px] mb-40 ml-50'>
-            <h1 className='text-[#e0e0e0] text-[1.5rem] font-bold'>What I've worked on</h1>
-          </div>
-          
+          {/* Project 1 */}
+          {/* Image Holder for Project 1 */}
           <div className='w-[400px] h-[250px] mb-50 ml-50 bg-[#b0bec5] z-1000'>
             <img 
               src="/miguel-portfolio/images/trainin-aas.jpg"
@@ -15,11 +16,15 @@ function Work() {
               className='w-full h-full object-cover'
             />
           </div>
+
+          {/* Image Border */}
           <div className='absolute w-[375px] h-[250px] mb-40 ml-45 bg-none border-[2px] border-[#fff]'></div>
 
+          {/* Project Title/Subtitle */}
           <h1 className='absolute mb-107 ml-160 text-[#e0e0e0] text-[1.25rem] font-semibold'>TrainIn</h1>
           <h1 className='absolute mb-96 ml-160 text-gray-400 text-[0.75rem]'>Automated Attendance System</h1>
 
+          {/* Project Description */}
           <div className='absolute w-[400px] h-[120px] bg-[#000e1e] text-justify ml-145 mb-60 p-3.5 z-1000'>
             <p className='flex text-[#e0e0e0] text-[0.750rem]'>
               TrainIn is an automated attendance system designed to record and track the attendance
@@ -28,10 +33,12 @@ function Work() {
             </p>
           </div>
 
+          {/* GitHub Link */}
           <a href="https://github.com/vnclrd/trainin-aas" target="_blank" rel="noopener noreferrer" className='absolute w-[25px] mb-102 ml-235'>
             <img src="/miguel-portfolio/images/logos/github.png" alt="GitHub" />
           </a>
 
+          {/* Technologies Used */}
           <div className='absolute text-gray-500 text-[0.60rem] ml-160 mb-22 gap-10'>
             <div className='flex gap-4'>
               <p>React Native</p>
@@ -42,9 +49,8 @@ function Work() {
             </div>
           </div>
 
-          <h1 className='absolute mt-37 ml-125 text-[#e0e0e0] text-[1.25rem] font-semibold'>InstaPrint</h1>
-          <h1 className='absolute mt-48 ml-111 text-gray-400 text-[0.75rem]'>Self-Service Printing Kiosk</h1>
-
+          {/* Project 2 */}
+          {/* Image Holder for Project 2 */}
           <div className='w-[400px] h-[250px] mt-60 bg-[#b0bec5] z-1000'>
             <img 
               src="/miguel-portfolio/images/instaprint.jpg"
@@ -52,12 +58,14 @@ function Work() {
               className='w-full h-full object-cover'
             />
           </div>
-
-          <a href="https://github.com/katrina110/instaprint-flask-" target="_blank" rel="noopener noreferrer" className='absolute w-[25px] mt-42 ml-68.5'>
-            <img src="/miguel-portfolio/images/logos/github.png" alt="GitHub" />
-          </a>
-
+          {/* Image Border */}
           <div className='absolute w-[375px] h-[250px] mt-70 ml-171 bg-none border-[2px] border-[#90494d]'></div>
+
+          {/* Project Title/Subtitle */}
+          <h1 className='absolute mt-37 ml-125 text-[#e0e0e0] text-[1.25rem] font-semibold'>InstaPrint</h1>
+          <h1 className='absolute mt-48 ml-111 text-gray-400 text-[0.75rem]'>Self-Service Printing Kiosk</h1>
+
+          {/* Project Description */}
           <div className='absolute w-[400px] h-[120px] bg-[#000e1e] text-justify ml-65 mt-85 p-3.5 z-1000'>
             <p className='flex text-[#e0e0e0] text-[0.75rem]'>
               Developed a full-stack printing kiosk that calculates document pricing
@@ -67,6 +75,12 @@ function Work() {
             </p>
           </div>
 
+          {/* GitHub Link */}
+          <a href="https://github.com/katrina110/instaprint-flask-" target="_blank" rel="noopener noreferrer" className='absolute w-[25px] mt-42 ml-68.5'>
+            <img src="/miguel-portfolio/images/logos/github.png" alt="GitHub" />
+          </a>
+
+          {/* Technologies Used */}
           <div className='absolute text-gray-500 text-[0.60rem] ml-68.5 mt-123 gap-10'>
             <div className='flex gap-4'>
               <p>Python</p>
@@ -77,9 +91,7 @@ function Work() {
               <p>MySQL</p>
             </div>
           </div>
-
         </div>
-        
     </div>
   )
 }
