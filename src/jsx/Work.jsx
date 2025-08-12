7c8815eeb4296cbe1fd655b532fb6ec15313266d
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-12 16:37:05 +0800

    Add technologies to works

--- src/jsx/Work.jsx
+++ src/jsx/Work.jsx
@@ -17,19 +17,29 @@ function Work() {
           </div>
           <div className='absolute w-[375px] h-[250px] mb-40 ml-45 bg-none border-[2px] border-[#00b4d8]'></div>
 
-          <h1 className='absolute mb-107 ml-161 text-[#e0e0e0] text-[1.25rem] font-semibold'>TrainIn</h1>
-          <h1 className='absolute mb-96 ml-161 text-[#e0e0e0] text-[0.75rem]'>Automated Attendance System</h1>
+          <h1 className='absolute mb-107 ml-160 text-[#e0e0e0] text-[1.25rem] font-semibold'>TrainIn</h1>
+          <h1 className='absolute mb-96 ml-160 text-gray-400 text-[0.75rem]'>Automated Attendance System</h1>
 
           <div className='absolute w-[400px] h-[120px] bg-[#000e1e] text-justify ml-145 mb-60 p-3.5 z-1000'>
-            <p className='flex text-[#e0e0e0] text-[0.75rem]'>
+            <p className='flex text-[#e0e0e0] text-[0.750rem]'>
               TrainIn is an automated attendance system designed to record and track the attendance
               of individuals at organized events. It uses NFC cards along with an external reader
               device to provide a seamless and efficient check-in and check-out process.
             </p>
           </div>
 
+          <div className='absolute text-gray-500 text-[0.60rem] ml-160 mb-22 gap-10'>
+            <div className='flex gap-4'>
+              <p>React Native</p>
+              <p>JavaScript</p>
+              <p>TypeScript</p>
+              <p>Tailwind CSS</p>
+              <p>SQLite</p>
+            </div>
+          </div>
+
           <h1 className='absolute mt-37 ml-125 text-[#e0e0e0] text-[1.25rem] font-semibold'>InstaPrint</h1>
-          <h1 className='absolute mt-48 ml-111 text-[#e0e0e0] text-[0.75rem]'>Self-Service Printing Kiosk</h1>
+          <h1 className='absolute mt-48 ml-111 text-gray-400 text-[0.75rem]'>Self-Service Printing Kiosk</h1>
 
           <div className='w-[400px] h-[250px] mt-60 bg-[#b0bec5] z-1000'>
             <img 
@@ -46,6 +56,17 @@ function Work() {
               back-end and utilized WebSocket-based payment tracking as well as cashless
               payment with GCash with integration with PayMongo.
             </p>
+          </div>
+
+          <div className='absolute text-gray-500 text-[0.60rem] ml-65 mt-123 gap-10'>
+            <div className='flex gap-4'>
+              <p>Python</p>
+              <p>JavaScript</p>
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>Bootstrap</p>
+              <p>MySQL</p>
+            </div>
           </div>
 
         </div>
