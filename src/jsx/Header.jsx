da2135822afeb94d2c4e6252caa33b696fa03340
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-13 19:08:17 +0800

    Adjust header responsiveness

--- src/jsx/Header.jsx
+++ src/jsx/Header.jsx
@@ -25,43 +25,39 @@ function Header() {
 
   return (
     <div
-      className={`
-        flex flex-row flex-nowrap items-center space-x-4
-        h-10 fixed top-[20px] z-[1000] shadow-lg
-        justify-center
+  className={`
+    flex flex-row flex-nowrap items-center space-x-4
+    h-15 w-[100%] fixed z-[1000]
+    justify-center bg-[#0d1b2a]
+    
+    // For screen sizes below 1200px (minimized/smaller screens)
+    left-1/2 -translate-x-1/2 transform
 
-        md:w-[100%]
-        
-        // For screen sizes other than lg and md
-        left-1/2 -translate-x-1/2 transform
-
-        md:left-1/2
-        md:-translate-x-1/2
-        md:transform
-
-        lg:w-[25%]
-        lg:left-[85%]
-        lg:transform-none
-        lg:border transition-[border-color,box-shadow] duration-500 ease-in-out
-        lg:shadow-none
-        
-        ${scrolled 
-          ? 'lg:border-[#e0e0e0] ' 
-          : 'border-transparent'
-        }
-      `}
-    >
-      <ul className="flex flex-row flex-nowrap items-center space-x-2 bg-transparent text-[0.75rem]">
-        <li className="inline-block bg-transparent">
+    min-[1400px]:w-[25%]
+    min-[1400px]:h-[7.5%]
+    min-[1400px]:left-[85%]
+    min-[1400px]:transform-none
+    min-[1400px]:shadow-none
+    min-[1400px]:top-5
+    min-[1400px]:bg-transparent
+    
+    ${scrolled 
+      ? 'max-[1199px]:shadow-lg max-[1199px]:transition-shadow max-[1199px]:duration-500 max-[1199px]:ease-in-out min-[1200px]:border-[#e0e0e0]' 
+      : 'min-[1200px]:border-transparent'
+    }
+  `}
+>
+      <ul className="flex flex-row flex-nowrap items-center space-x-2 text-[0.75rem]">
+        <li className="inline-block">
           <a href="#" onClick={(e) => { e.preventDefault(); scrollToAbout() }} className="no-underline text-[#e0e0e0] m-5 bg-transparent">About</a>
         </li>
-        <li className="inline-block bg-transparent">
+        <li className="inline-block">
           <a href="#" onClick={(e) => { e.preventDefault(); scrollToWork() }} className="no-underline text-[#e0e0e0] m-5 bg-transparent">Work</a>
         </li>
-        <li className="inline-block bg-transparent">
+        <li className="inline-block">
           <a href="#" onClick={(e) => { e.preventDefault(); scrollToExperience() }} className="no-underline text-[#e0e0e0] m-5 bg-transparent">Experience</a>
         </li>
-        <li className="inline-block bg-transparent">
+        <li className="inline-block">
           <a href="#" className="no-underline text-[#e0e0e0] m-5 bg-transparent">Contact</a>
         </li>
       </ul>
