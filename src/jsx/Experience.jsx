a7602b38feb84c4d42788cdde7ca9f0d79e02c7b
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-18 19:42:45 +0800

    Set up mobile layout for experience section

--- src/jsx/Experience.jsx
+++ src/jsx/Experience.jsx
@@ -6,113 +6,22 @@ function Experience() {
       <div className="block lg:hidden px-4 py-8">
         
         {/* Section Title */}
-        <h1 className='text-[#e0e0e0] text-[1.5rem] font-bold mb-8 text-center'>
+        <h1 className='text-[#e0e0e0] text-[1.5rem] font-bold mt-15 mb-8 text-center'>
           Where I've Worked
         </h1>
 
-        {/* Project 1 - TrainIn */}
+        {/* Experience 1 */}
         <div className="mb-12">
-          
           {/* Project Image */}
-          <div className='relative mb-4'>
-            <div className='w-full max-w-sm mx-auto h-[200px] bg-[#b0bec5]'>
-              <img 
-                src="/miguel-portfolio/images/trainin-aas.jpg"
-                alt="TrainIn"
-                className='w-full h-full object-cover'
-              />
-            </div>
-            <div className='absolute inset-0 w-full max-w-sm mx-auto h-[200px] border-[2px] border-[#fff]'></div>
-          </div>
-
-          {/* Project Info */}
-          <div className='text-center mb-4'>
-            <div className='flex items-center justify-center gap-2 mb-2'>
-              <h2 className='text-[#e0e0e0] text-[1.25rem] font-semibold'>TrainIn</h2>
-              <a href="https://github.com/vnclrd/trainin-aas" target="_blank" rel="noopener noreferrer" className='w-[20px] h-[20px]'>
-                <img src="/miguel-portfolio/images/logos/github.png" alt="GitHub" />
-              </a>
-            </div>
-            <p className='text-gray-400 text-[0.75rem] mb-4'>Automated Attendance System</p>
-          </div>
-
-          {/* Project Description */}
-          <div className='bg-[#000e1e] p-4 mb-4 rounded'>
-            <p className='text-[#e0e0e0] text-[0.75rem] text-center'>
-              TrainIn is an automated attendance system designed to <span className='text-[#00b4d8] font-semibold'>record and track attendance</span> before and after a session.
-              It uses NFC chips along with an external reader device to provide a seamless and efficient check-in and check-out process.
-            </p>
-          </div>
-
-          {/* Technologies */}
-          <div className='text-center'>
-            <div className='flex flex-wrap justify-center gap-2 text-gray-500 text-[0.60rem]'>
-              <span>React Native</span>
-              <span>•</span>
-              <span>JavaScript</span>
-              <span>•</span>
-              <span>TypeScript</span>
-              <span>•</span>
-              <span>Tailwind CSS</span>
-              <span>•</span>
-              <span>SQLite</span>
-            </div>
-          </div>
+          <div className='relative mb-4 w-full max-w-sm mx-auto h-[200px] bg-[#b0bec5]'></div>
         </div>
 
-        {/* Project 2 - InstaPrint */}
+        {/* Experience 2 */}
         <div className="mb-12">
-          
           {/* Project Image */}
-          <div className='relative mb-4'>
-            <div className='w-full max-w-sm mx-auto h-[200px] bg-[#b0bec5]'>
-              <img 
-                src="/miguel-portfolio/images/instaprint.jpg"
-                alt="InstaPrint"
-                className='w-full h-full object-cover'
-              />
-            </div>
-            <div className='absolute inset-0 w-full max-w-sm mx-auto h-[200px] border-[2px] border-[#90494d]'></div>
-          </div>
-
-          {/* Project Info */}
-          <div className='text-center mb-4'>
-            <div className='flex items-center justify-center gap-2 mb-2'>
-              <h2 className='text-[#e0e0e0] text-[1.25rem] font-semibold'>InstaPrint</h2>
-              <a href="https://github.com/katrina110/instaprint-flask-" target="_blank" rel="noopener noreferrer" className='w-[20px] h-[20px]'>
-                <img src="/miguel-portfolio/images/logos/github.png" alt="GitHub" />
-              </a>
-            </div>
-            <p className='text-gray-400 text-[0.75rem] mb-4'>Self-Service Printing Kiosk</p>
-          </div>
-
-          {/* Project Description */}
-          <div className='bg-[#000e1e] p-4 mb-4 rounded'>
-            <p className='text-[#e0e0e0] text-[0.75rem] text-center'>
-              Developed a full-stack printing kiosk that calculates document pricing
-              based on page count, color, and paper size. Utilized <span className='text-[#00b4d8] font-semibold'>Python + Flask</span> and <span className='text-[#00b4d8] font-semibold'>WebSocket-based</span> payment
-              tracking as well as cashless payments with <span className='text-[#00b4d8] font-semibold'>GCash</span> with integration with <span className='text-[#00b4d8] font-semibold'>PayMongo</span>.
-            </p>
-          </div>
-
-          {/* Technologies */}
-          <div className='text-center'>
-            <div className='flex flex-wrap justify-center gap-2 text-gray-500 text-[0.60rem]'>
-              <span>Python</span>
-              <span>•</span>
-              <span>JavaScript</span>
-              <span>•</span>
-              <span>HTML</span>
-              <span>•</span>
-              <span>CSS</span>
-              <span>•</span>
-              <span>Bootstrap</span>
-              <span>•</span>
-              <span>MySQL</span>
-            </div>
-          </div>
+          <div className='relative mb-4 w-full max-w-sm mx-auto h-[200px] bg-[#b0bec5]'></div>
         </div>
-
+        
       </div>
 
       {/* Desktop Layout */}
