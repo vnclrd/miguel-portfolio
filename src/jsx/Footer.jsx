48a04519156995f306478e72afcb42bdf91d674c
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-18 23:15:36 +0800

    Update footer description

--- src/jsx/Footer.jsx
+++ src/jsx/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <div className="flex w-full h-[120px] justify-center bg-[#000e1e]">
       <div className="flex flex-col items-center justify-center">
         <p className="text-[#e0e0e0] leading-4 mb-3 text-[0.6rem] font-thin text-center">
-          This website was built with React and Tailwind CSS
+          This website was developed with React and Tailwind CSS
           <br/>
           by Miguel Ivan Calarde
         </p>
